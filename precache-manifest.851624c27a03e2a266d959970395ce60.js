dbe35ed2797dd19ef659da53f14880aa9cb1948b
Tolga Kurtulus | Odeon Technology <tolga.kurtulus@odeontechnology.com>
2023-09-01 13:33:21 +0300

    Updates

---
+++ precache-manifest.851624c27a03e2a266d959970395ce60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "93e82e3468103fc439de",
+    "url": "/react-apollographql/static/css/main.c616a28d.chunk.css"
+  },
+  {
+    "revision": "93e82e3468103fc439de",
+    "url": "/react-apollographql/static/js/main.93e82e34.chunk.js"
+  },
+  {
+    "revision": "0a58e1c4ae58131f766e",
+    "url": "/react-apollographql/static/js/1.0a58e1c4.chunk.js"
+  },
+  {
+    "revision": "7d1de1d745d37bfdd785",
+    "url": "/react-apollographql/static/js/2.7d1de1d7.chunk.js"
+  },
+  {
+    "revision": "3ea85b447bc7442190b1",
+    "url": "/react-apollographql/static/js/runtime~main.3ea85b44.js"
+  },
+  {
+    "revision": "9618ea6c6b2f8e147edd7d106d6c6dc9",
+    "url": "/react-apollographql/index.html"
+  }
+];
